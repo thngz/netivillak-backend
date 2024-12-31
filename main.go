c87fe80ab49ffc9ff3b34b75c00af4dd0b2dbbc8
gkiviv <georgkivivali7@gmail.com>
2024-12-31 14:10:10 +0200

    format

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ import (
 )
 
 type Question struct {
-	Clue   string `json:"clue"`
-	Answer string `json:"answer"`
-	Points int    `json:"points"`
-	Category string    `json:"category"`
-	Col    int    `json:"col"`
-	Row    int    `json:"row"`
+	Clue     string `json:"clue"`
+	Answer   string `json:"answer"`
+	Points   int    `json:"points"`
+	Category string `json:"category"`
+	Col      int    `json:"col"`
+	Row      int    `json:"row"`
 	// Opened bool   `json:"opened"`
 }
 
